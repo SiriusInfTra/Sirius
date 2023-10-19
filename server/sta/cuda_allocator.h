42b4ffcb99cac6122b0d0f4d99f2cbafd425709a
Yankui Wang <yankui87@sjtu.edu.cn>
2023-10-19 14:20:53 +0800

    mempool: merge hpp

--- server/sta/cuda_allocator.h
+++ server/sta/cuda_allocator.h
@@ -10,10 +10,102 @@
 #include <cstddef>
 #include <cuda_runtime_api.h>
 
-#include "mempool.hpp"
+
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/sync/interprocess_mutex.hpp>
+#include <boost/container/map.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/functional/hash.hpp>
+#include <boost/thread/lock_guard.hpp>
+
+
+#include <cuda_runtime_api.h>
+
+#include <thread>
+#include <chrono>
+#include <iostream>
+
 
 namespace colserve {
 namespace sta {
+
+namespace bip = boost::interprocess;
+
+class CUDAMemPoolImpl {
+public:
+  struct MemPoolConfig {
+    int cuda_device;
+    size_t cuda_memory_size;
+    std::string shared_memory_name;
+    size_t shared_memory_size;
+  };
+  struct PoolEntry {
+    void *addr;
+    std::size_t nbytes;
+  };
+
+private:
+  using Addr2EntryType = std::pair<std::ptrdiff_t, bip::managed_shared_memory::handle_t>;
+  using Addr2EntryAllocator = bip::allocator<Addr2EntryType, bip::managed_shared_memory::segment_manager>;
+  using Addr2Entry = boost::unordered_map<std::ptrdiff_t, bip::managed_shared_memory::handle_t, boost::hash<std::ptrdiff_t>, std::equal_to<>, Addr2EntryAllocator>;
+
+  using Size2EntryType = std::pair<const size_t, bip::managed_shared_memory::handle_t>;
+  using Size2EntryAllocator = bip::allocator<Size2EntryType, bip::managed_shared_memory::segment_manager>;
+  using Size2Entry = boost::container::multimap<size_t, bip::managed_shared_memory::handle_t, std::less<>, Size2EntryAllocator>;
+
+  using RefCount = int;
+  using EntryHandle = bip::managed_shared_memory::handle_t;
+
+  struct PoolEntryHandle {
+    std::ptrdiff_t addr_offset;
+    std::size_t nbytes;
+    EntryHandle prev;
+    EntryHandle next;
+    bool allocate;
+  };
+
+  bip::managed_shared_memory segment_;
+  bip::interprocess_mutex *mutex_;
+  Addr2Entry *addr2entry_;
+  Size2Entry *size2entry_;
+  RefCount *ref_count_;
+  MemPoolConfig config_;
+  PoolEntryHandle *empty_;
+  cudaIpcMemHandle_t *cuda_mem_handle_;
+
+  bool master_;
+  void *devPtr_{};
+
+  inline PoolEntryHandle *GetEntry(EntryHandle handle);
+
+  inline bip::managed_shared_memory::handle_t GetHandle(PoolEntryHandle *entry);
+
+  inline void UpdateFreeEntrySize(Size2Entry::iterator iter, PoolEntryHandle *entry, size_t newSize);
+
+  inline void UpdateEntryAddr(const Addr2Entry::iterator &iter, std::ptrdiff_t newAddr);
+
+  inline void ConnectPoolEntryHandle(PoolEntryHandle *eh1, PoolEntryHandle *eh2);
+
+  inline void CheckMemPool();
+
+  std::shared_ptr<PoolEntry> MakeSharedPtr(PoolEntryHandle *eh);
+
+public:
+
+  explicit CUDAMemPoolImpl(MemPoolConfig config);
+
+  ~CUDAMemPoolImpl();
+
+  std::shared_ptr<PoolEntry> Alloc(std::size_t nbytes);
+
+private:
+  void Free(PoolEntryHandle *entry);
+
+};
+
+
 class CUDAMemPool {
  public:
   using PoolEntry = CUDAMemPoolImpl::PoolEntry;
