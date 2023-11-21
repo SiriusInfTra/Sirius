877933d5f8c2efb8d544ca81bd870cc07ec89ee1
Jiali Wang <wangjiali@sjtu.edu.cn>
2023-11-21 18:36:20 +0800

    overwrite  tensor is_cuda

--- python/torch_col/__init__.py
+++ python/torch_col/__init__.py
@@ -1,3 +1,7 @@
+def __setup_coltensor():
+    import torch
+    torch.Tensor.is_cuda = property(lambda self: self.device.type == 'cuda')
+
 def __initialize():
     import os, sys
     import ctypes
@@ -9,7 +13,7 @@ def __initialize():
 
     if os.environ.get('USE_SHARED_TENSOR', '0') == '1':
         ctypes.CDLL(os.path.join(lib_path, 'libtorch_col_tensor.so'), ctypes.RTLD_GLOBAL)
-
+        __setup_coltensor()
 
 __initialize()
 from ._C import *
