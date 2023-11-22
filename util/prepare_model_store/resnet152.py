b4997229f537a2dcbd7d1024ca1ca704f633389b
Jiali Wang <wangjiali@sjtu.edu.cn>
2023-11-22 15:45:57 +0800

    tvm script

---
+++ util/prepare_model_store/resnet152.py
@@ -0,0 +1,26 @@
+import torch
+from torchvision import models
+import onnx
+from tvm.driver import tvmc
+import tempfile
+import tarfile
+import pathlib
+
+batch_size = 4
+model_store = "models"
+tmp_dir = tempfile.gettempdir()
+
+resnet152 = models.resnet152(weights=models.ResNet152_Weights.DEFAULT).eval()
+torch.onnx.export(resnet152, torch.rand(batch_size, 3, 224, 224), 
+                  f"{tmp_dir}/resnet152.onnx", verbose=True,
+                  input_names=["input"], output_names=["output"], export_params=True,
+                  dynamic_axes={'input':[0], 'output':[0]})
+
+tvmc_model = tvmc.load(f"{tmp_dir}/resnet152.onnx", 
+                       shape_dict={'input':[batch_size, 3, 224, 224]})
+tvmc.compile(tvmc_model=tvmc_model, target='cuda -arch=sm_70', package_path=f"{tmp_dir}/resnet152-tvm.tar")
+
+
+model_store_path = f'{model_store}/resnet152-b{batch_size}' 
+pathlib.Path(model_store_path).mkdir(parents=True, exist_ok=True)
+tarfile.open(f"{tmp_dir}/resnet152-tvm.tar").extractall(model_store_path)
