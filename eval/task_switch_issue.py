c7b39354ad3f6cea673700933351893e150615f4
Yankui Wang <yankui87@sjtu.edu.cn>
2023-12-07 00:03:56 +0800

    more friendly log dir for taskswitch issue

--- eval/task_switch_issue.py
+++ eval/task_switch_issue.py
@@ -7,7 +7,7 @@ import time
 set_global_seed(42)
 
 
-def run(system: System, hyper_workload:HyperWorkload, num_model:int, req_per_sec:int, tag:str):
+def run(system: System, hyper_workload:HyperWorkload, num_model:int, req_per_sec:int, tag:str, time_stamp: str):
     InferModel.reset_model_cnt()
     # hyper_workload.set_infer_workloads(PoissonInferWorkload(
     #     poisson_params=zip(InferModel.get_model_list("resnet152", num_model), 
@@ -18,7 +18,7 @@ def run(system: System, hyper_workload:HyperWorkload, num_model:int, req_per_sec
     # hyper_workload.set_train_workload(None)
     infer_model_config = System.InferModelConfig(f"resnet152[{num_model}]", "resnet152", "1")
     req_per_sec_str = "%.2d" % req_per_sec if isinstance(req_per_sec, int) else "%.2f" % req_per_sec
-    system.launch("taskswitch-issue", f"{tag}-{req_per_sec_str}", time_stamp=True, infer_model_config=infer_model_config)
+    system.launch(f"taskswitch-issue-{time_stamp}", f"{tag}-{req_per_sec_str}", time_stamp=False, infer_model_config=infer_model_config)
     hyper_workload.set_infer_workloads(PoissonInferWorkload(
         poisson_params=list(zip(InferModel.get_model_list("resnet152", num_model), [PoissonParam(0, req_per_sec / num_model)] * num_model)),
         duration=60, 
@@ -38,26 +38,28 @@ def main():
         (1, 1,   97),
         (1, 2,   96),
         (1, 4,   92),
-        (1, 8,   84),
-        (1, 16,  76),
+        (1, 8,   90),
+        (1, 16,  84),
+        (1, 32,  76),
     ]
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M")
     for num_model, req_per_sec, train_thread_percent in parmas:
         system = System(mode=System.ServerMode.Normal, use_sta=False, mps=True, train_mps_thread_percent=train_thread_percent)
         workload = HyperWorkload(concurrency=1, duration=60, delay_before_infer=10)
-        run(system, workload, num_model, req_per_sec, "ideal")
+        run(system, workload, num_model, req_per_sec, "ideal", timestamp)
 
     system = System(mode=System.ServerMode.TaskSwitchL1, use_sta=False, mps=False)
     workload = HyperWorkload(concurrency=32, duration=60, delay_before_infer=30)
-    run(system, workload, 1, 0.1, "taskswitch")
-    run(system, workload, 1, 0.2, "taskswitch")
-    run(system, workload, 1, 0.5, "taskswitch")
-    run(system, workload, 1, 1, "taskswitch")
-    run(system, workload, 1, 1, "taskswitch")
-    run(system, workload, 1, 2, "taskswitch")
-    run(system, workload, 1, 4, "taskswitch")
-    run(system, workload, 1, 8, "taskswitch")
-    run(system, workload, 1, 16, "taskswitch")
-    run(system, workload, 1, 32, "taskswitch")
+    run(system, workload, 1, 0.1, "taskswitch", timestamp)
+    run(system, workload, 1, 0.2, "taskswitch", timestamp)
+    run(system, workload, 1, 0.5, "taskswitch", timestamp)
+    run(system, workload, 1, 1, "taskswitch", timestamp)
+    run(system, workload, 1, 1, "taskswitch", timestamp)
+    run(system, workload, 1, 2, "taskswitch", timestamp)
+    run(system, workload, 1, 4, "taskswitch", timestamp)
+    run(system, workload, 1, 8, "taskswitch", timestamp)
+    run(system, workload, 1, 16, "taskswitch", timestamp)
+    run(system, workload, 1, 32, "taskswitch", timestamp)
 
 
 if __name__ == "__main__":
