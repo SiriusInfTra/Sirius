af0c75517c4684ec6b6c76650db364aaf27a2387
wangjl <anlarry@foxmail.com>
2024-01-03 10:24:49 +0800

    minor

--- eval/runner/runner.py
+++ eval/runner/runner.py
@@ -208,7 +208,7 @@ class System:
         self.cmd_trace.append(" ".join(cmd))
         print("\n---------------------------\n")
         print(" ".join(cmd))
-        print(f"  --> [server-log] {server_log}  |  [server-profile] {self.log_dir}/{self.profile_log}\n")
+        print(f"  --> [server-log] {server_log}  |  [server-profile] {profile_log}\n")
 
         with open(server_log, "w") as log_file:
             self.server = subprocess.Popen(cmd, stdout=log_file, stderr=subprocess.STDOUT, env=os.environ.copy())
