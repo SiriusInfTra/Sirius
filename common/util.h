4af6871bbf0f1b948193dca7eb0a02ff011fa5ee
wangjl <anlarry@foxmail.com>
2024-03-25 22:58:50 +0800

    minor

--- common/util.h
+++ common/util.h
@@ -20,19 +20,6 @@
   } \
   } while (0)
 
-// #define CU_CALL(func) do { \
-//   auto t0 = std::chrono::steady_clock::now(); \
-//   auto error = func; \
-//   auto t1 = std::chrono::steady_clock::now(); \
-//   if (error != CUDA_SUCCESS) { \
-//     const char *errMsg; cuGetErrorString(error, &errMsg); \
-//     LOG(FATAL) << #func << " " << errMsg; \
-//     exit(EXIT_FAILURE); \≈
-//   } else { \
-//     LOG_IF(INFO, false) << #func << " cost " << std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() << " us"; \
-//   } \
-//   } while (0)
-
 #define CU_CALL(func) \
   do { \
     auto err = func; \
