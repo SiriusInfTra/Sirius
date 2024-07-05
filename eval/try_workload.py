2e05bf3c163cca0e7ab88db8e074675ae37d0177
wangjl <anlarry@foxmail.com>
2024-07-05 21:31:11 +0800

    eval script: minor

--- eval/try_workload.py
+++ eval/try_workload.py
@@ -37,6 +37,7 @@ if run_colsys:
         'mode': System.ServerMode.ColocateL1,
         'use_sta': True,
         'mps': True,
+        'skip_set_mps_thread_percent': True,
         'use_xsched': True,
         'dynamic_sm_partition': True,
         'ondemand_adjust': True,
@@ -49,6 +50,7 @@ if run_colsys:
     }
 
     for wkld_name, wkld in normal_wklds.items():
+        # with mps_thread_percent(50):
         with mps_thread_percent(None):
             client_model_list, server_model_config = InferModel.get_multi_model(
                 run_comm.UniformConfig_v2.model_list, run_comm.UniformConfig_v2.num_model, 1)
@@ -59,6 +61,7 @@ if run_colsys:
                         "try-workload", f"colsys-{wkld_name}")
             
     for wkld_name, wkld in skew_wklds.items():
+        # with mps_thread_percent(50):
         with mps_thread_percent(None):
             client_model_list, server_model_config = InferModel.get_multi_model(
                 run_comm.SkewedConfig_v2.model_list, run_comm.SkewedConfig_v2.num_model, 1)
@@ -99,6 +102,7 @@ if run_static_partition:
         system_config, workload_config = item
 
         for wkld_name, wkld in normal_wklds.items():
+            # with mps_thread_percent(50):
             with mps_thread_percent(None):
                 client_model_list, server_model_config = InferModel.get_multi_model(
                     run_comm.UniformConfig_v2.model_list, run_comm.UniformConfig_v2.num_model, 1)
@@ -111,6 +115,7 @@ if run_static_partition:
                             "try-workload", f"static-partition-{tag}-{wkld_name}")
         
         for wkld_name, wkld in skew_wklds.items():
+            # with mps_thread_percent(50):
             with mps_thread_percent(None):
                 client_model_list, server_model_config = InferModel.get_multi_model(
                     run_comm.SkewedConfig_v2.model_list, run_comm.SkewedConfig_v2.num_model, 1)
@@ -121,3 +126,72 @@ if run_static_partition:
                                 **system_config)
                 run_comm.run(system, workload, server_model_config,
                              "try-workload", f"static-partition-{tag}-{wkld_name}")
+
+
+
+# ==========================================================================
+# ==========================================================================
+
+# import os, sys, re
+# import pathlib
+# import pandas as pd
+
+
+# def parse_test_unit_name(name:str):
+#     m = re.match(r'(.*)-((Normal|Skew).*)', name)
+#     if m:
+#         return m.group(1), m.group(2)
+#     else:
+#         raise ValueError(f'Invalid test unit name: {name}')
+
+
+# def get_perf(log_dir:pathlib.Path):
+#     try:
+#         workload_log = log_dir / 'workload-log'
+#         train_thpt_log = log_dir / 'train_thpt'
+#         if not workload_log.exists() or not train_thpt_log.exists():
+#             log_dir = pathlib.Path(str(log_dir) + '-retry-0')
+#             workload_log = log_dir / 'workload-log'
+#             train_thpt_log = log_dir / 'train_thpt'
+#         with open(workload_log, 'r') as f:
+#             p99 = re.search(r'p99 ([^ ]+) ', f.readlines()[2])
+#         with open(train_thpt_log, 'r') as f:
+#             thpt = re.search(r'.*epoch e2e thpt: ([^ ]+) .*', f.read())
+#         return float(p99.group(1)), float(thpt.group(1))
+#     except Exception:
+#         return None, None
+
+
+# # log_root = 'log/try-workload-20240630-2330'
+# # log_root = 'log/try-workload-20240703-0901'
+# log_root = 'log/try-workload-20240704-1001'
+
+# df = pd.DataFrame({
+#     'Unit': [],
+#     'colsys-P99': [],
+#     'SP-F-P99': [],
+#     'SP-I-P99': [],
+
+#     'colsys-Thpt': [],
+#     'SP-F-Thpt': [],
+#     'SP-I-Thpt': [],
+# })
+
+# test_units = [unit.name for unit in pathlib.Path(log_root).glob('*') 
+#               if 'retry' not in unit.name]
+# test_units = [parse_test_unit_name(unit)[1] for unit in test_units]
+# test_units = list(set(test_units))
+
+# for unit in test_units:
+#     colsys_p99, colsys_thpt = get_perf(pathlib.Path(log_root) / f'colsys-{unit}')
+#     sp_f_p99, sp_f_thpt = get_perf(pathlib.Path(log_root) / f'static-partition-F-{unit}')
+#     sp_i_p99, sp_i_thpt = get_perf(pathlib.Path(log_root) / f'static-partition-I-{unit}')
+#     df = pd.concat([df, pd.DataFrame(
+#         [[unit, colsys_p99, sp_f_p99, sp_i_p99, colsys_thpt, sp_f_thpt, sp_i_thpt]], 
+#         columns=df.columns
+#     )], ignore_index=True)
+
+# df = df.sort_values(by=['Unit'])
+
+# print(df.to_string())
+    
