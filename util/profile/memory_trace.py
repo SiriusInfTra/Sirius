6bf0b960c613201db00d6198602e1ff26a55e40d
wangjl <anlarry@foxmail.com>
2024-08-13 15:03:21 +0800

    util: memory trace script

--- util/profile/memory_trace.py
+++ util/profile/memory_trace.py
@@ -19,63 +19,70 @@ begin_time = args.b
 end_time = args.e
 outdir = args.o
 
-memory_info_str = ""
+max_device_num = 32
+memory_info_strs = [None] * max_device_num
 parse_mem = False
+device_id = None
 with open(log) as f:
     for line in f.readlines():
-        if '[Memory Info]' in line:
+        memory_info_match = re.search(r'\[Memory Info \| Device (\d+)\]', line)
+        if memory_info_match:
+            device_id = int(memory_info_match.group(1))
+            memory_info_strs[device_id] = ''
             parse_mem = True
             continue
         if len(line.strip()) == 0:
             parse_mem = False
+            device_id = None
 
         if parse_mem:
-            memory_info_str += line
+            memory_info_strs[device_id] += line
 
-df = pd.read_csv(StringIO(memory_info_str), sep=',', header=0, index_col=0)
+num_devices = len([mem for mem in memory_info_strs if mem is not None])
 
+ax_width = 6
+ax_height = 4
+hmargin = 0.5
+fig, axes = plt.subplots(num_devices, 1, 
+                         figsize=(ax_width, ax_height*num_devices + hmargin*(num_devices-1)))
 
-memory_transform = lambda x: float(re.search(r'(\d+\.\d+) Mb', x).group(1))
+for i in range(num_devices):
+    ax = axes[i]
+    df = pd.read_csv(StringIO(memory_info_strs[i]), sep=',', header=0, index_col=0)
 
-df.columns = df.columns.str.strip()
-# print(df.columns)
-timeline = df.index - df.index[0]
-total_mem = df['TotalMem'].apply(memory_transform)
-train_all_mem = df['TrainAllMem'].apply(memory_transform)
-infer_mem = df['InferMem'].apply(memory_transform)
+    memory_transform = lambda x: float(re.search(r'(\d+\.\d+) Mb', x).group(1))
 
+    df.columns = df.columns.str.strip()
+    # print(df.columns)
+    timeline = df.index - df.index[0]
+    total_mem = df['TotalMem'].apply(memory_transform)
+    train_all_mem = df['TrainAllMem'].apply(memory_transform)
+    infer_mem = df['InferMem'].apply(memory_transform)
 
-total_mem = max(total_mem)
+    total_mem = max(total_mem)
 
-if args.train_all:
-    train_mem = train_all_mem
+    if args.train_all:
+        train_mem = train_all_mem
 
-fig, ax = plt.subplots()
-infer_mem, train_mem = map(np.array, [infer_mem, train_mem])
-# invert_train_mem = total_mem - train_mem
-infer_train_mem = infer_mem + train_mem
-
-
-ax.plot(timeline, infer_mem, label='Infer', linewidth=0.2)
-ax.fill_between(timeline, 0, infer_mem, alpha=0.3)
-
-ax.plot(timeline, infer_train_mem, label='Train', linewidth=0.2)
-ax.fill_between(timeline, infer_mem, infer_train_mem, alpha=0.3)
-
-# if args.event:
-#     ax.vlines([t for t, e in events], 0.0, total_mem, color='black', linewidth=0.5, linestyles='dashed')
+    infer_mem, train_mem = map(np.array, [infer_mem, train_mem])
+    # invert_train_mem = total_mem - train_mem
+    infer_train_mem = infer_mem + train_mem
 
 
-# print([t for t, e in events])
-# ax.vlines([t for t, e in events], 0, total_mem, 
-#           color='black', linewidth=0.5, linestyles='dashed')
+    ax.plot(timeline, infer_mem, label='Infer', linewidth=0.2)
+    ax.fill_between(timeline, 0, infer_mem, alpha=0.3)
 
+    ax.plot(timeline, infer_train_mem, label='Train', linewidth=0.2)
+    ax.fill_between(timeline, infer_mem, infer_train_mem, alpha=0.3)
 
-ax.set_ylim(0, total_mem)
-ax.set_xlim(0,
-            min(max(timeline), end_time - begin_time))
+    ax.set_ylim(0, total_mem)
+    ax.set_xlim(0,
+                min(max(timeline), end_time - begin_time))
 
-ax.set_xlabel("Time (ms)", fontsize=14)
-ax.set_ylabel("GPU Memory (MB)",fontsize=14)
-plt.legend()
+    ax.set_xlabel("Time (ms)", fontsize=14)
+    ax.set_ylabel("GPU Memory (MB)",fontsize=14)
+    ax.legend()
+
+# plt.legend()
+
 plt.savefig(f'{outdir}/memory-trace.svg')
