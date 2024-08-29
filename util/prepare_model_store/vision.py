55c275490f254b07dac8dcc7344871610048c18a
Yankui Wang <yankui87@sjtu.edu.cn>
2024-08-29 20:24:27 +0800

    add prepare model

--- util/prepare_model_store/vision.py
+++ util/prepare_model_store/vision.py
@@ -9,26 +9,37 @@ import pathlib
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
+parser.add_argument("--model-name", type=str, required=True)
 parser.add_argument("--batch-size", type=int, default=1)
 parser.add_argument("--tune", action="store_true")
 parser.add_argument("--model-store", type=str, default="server/models")
 args = parser.parse_args()
 
+model_name = cast(str, args.model_name)
 batch_size = cast(int, args.batch_size)
 model_store = cast(str, args.model_store)
 tune = cast(bool, args.tune)
 
 tmp_dir = tempfile.gettempdir()
-onnx_path = f"{tmp_dir}/modeltmp-densenet161-b{batch_size}.onnx"
-tvm_path = f"{tmp_dir}/modeltmp-densenet161-b{batch_size}.tar"
-model_store_path = f'{model_store}/densenet161-b{batch_size}' 
+onnx_path = f"{tmp_dir}/modeltmp-{model_name}-b{batch_size}.onnx"
+tvm_path = f"{tmp_dir}/modeltmp-{model_name}-b{batch_size}.tar"
+model_store_path = f'{model_store}/{model_name}-b{batch_size}' 
 tune_records = os.path.join(os.path.dirname(__file__), 'tune_records',
-                            f'densenet161-b{batch_size}-tune.json')
+                            f'{model_name}-b{batch_size}-tune.json')
 
 print('Preparing model...')
 if not os.path.exists(onnx_path):
-    densenet161 = models.densenet169(weights=models.DenseNet169_Weights.DEFAULT).eval()
-    torch.onnx.export(densenet161, torch.rand(batch_size, 3, 224, 224), 
+    if model_name == 'densenet161':
+        model = models.densenet161(weights=models.DenseNet161_Weights.DEFAULT)
+    elif model_name == 'resnet152':
+        model = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
+    elif model_name == 'efficientnet_v2_s':
+        model = models.efficientnet_v2_s(weights=models.EfficientNet_V2_S_Weights.DEFAULT)
+    else:
+        raise ValueError(f"Model {model_name} not supported")
+    model = model.eval()
+    model = models.densenet169(weights=models.DenseNet169_Weights.DEFAULT).eval()
+    torch.onnx.export(model, torch.rand(batch_size, 3, 224, 224), 
                     onnx_path, verbose=True,
                     input_names=["input"], output_names=["output"], export_params=True)
 
@@ -36,7 +47,8 @@ print('Tuning model...')
 tvmc_model = tvmc.load(onnx_path, shape_dict={'input':[batch_size, 3, 224, 224]})
 
 if tune:
-    tvmc.tune(tvmc_model=tvmc_model, target='cuda', 
+    if not os.path.exists(tune_records):
+        tvmc.tune(tvmc_model=tvmc_model, target='cuda', 
             tuning_records=tune_records, 
             enable_autoscheduler=False)
 else:
