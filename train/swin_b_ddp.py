59a96f8ae02bd83435d72353f0450716fd039c72
Yankui Wang <yankui87@sjtu.edu.cn>
2024-09-22 23:06:08 +0800

    add swin_b

--- train/swin_b_ddp.py
+++ train/swin_b_ddp.py
@@ -1,9 +1,10 @@
+import contextlib
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as torch_dist
 import torch.multiprocessing as torch_mp
 from torch import nn
-from torchvision import models
+import swin_b_tiny
 from torch.utils.data import TensorDataset, DataLoader
 import argparse
 import os, sys
@@ -12,16 +13,21 @@ from torch_col import MemoryPool, EventManager, TrainMode, HookMode
 from torch_col import DynamicBatchDataset
 import torch_col.trainer
 import torch_col.xsched
-from typing import Optional
+from typing import Optional, cast
 import time
 from torch.utils.data import Dataset
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, TensorDataset
 
 
 def setup(rank, world_size):
     cuda_index = torch.cuda._get_nvml_device_index(0)
     torch_col.info(f'[Train rank {rank} PID {os.getpid()} world size {world_size}] setup')
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = str(12345 + cuda_index)
+    os.environ['MASTER_PORT'] = str(22345 + cuda_index)
     torch_col.setup_colocate_training(rank, world_size, True, True)
 
 def cleanup():
@@ -34,6 +40,8 @@ def train(rank:int, world_size:int,
           global_batch_size: Optional[int] = None):
     setup(rank, world_size)
     torch_col.init_train_info(batch_size, batch_size, model_name='swin_b_ddp')
+    print(f'Env: {os.environ}')
+    print(f'Real Data: {real_data}')
 
     hook_mode = torch_col.get_hook_mode()
     
@@ -41,24 +49,40 @@ def train(rank:int, world_size:int,
         torch_col.tag_model_start()
 
 
-    model = models.swin_b(weights=models.Swin_B_Weights.DEFAULT).cuda()
+    model = swin_b_tiny.SwinTransformer(
+        input_shape=(32, 32, 3),
+        patch_size=(2, 2),
+        qkv_bias=True,
+        num_classes=100,
+        embed_dim=128,
+        num_heads=8,
+        num_mlp=256,
+        window_size=2,
+        shift_size=1,
+        dropout_rate=0.1
+    ).cuda()
+
     # for param in model.parameters():
     #     param.requires_grad = False
     if os.environ.get('COL_IMAGENET', '0') == '1':
+        num_class = 100
+    elif os.environ.get('COL_CIFAR100', '0') == '1':
         num_class = 100
     elif real_data:
         num_class = 37
     else:
         num_class = 10
+    print(f"num_class = {num_class}.")
     if real_data:
         model.head = torch.nn.Linear(model.head.in_features, num_class).cuda()
     model = DDP(model, device_ids=[rank])
 
     print(f"Train params memory usage: {torch_col.MemoryPool.get_memory_usage() * 1024:.2f}M")
 
-    criterion = nn.CrossEntropyLoss().cuda(rank)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, weight_decay=0.05, momentum=0.9)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.1).cuda(rank)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-5)
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
     # optimizer, scheduler = torch_col.get_imagenet_optimizer_and_scheduler(model, global_batch_size, 90, 5)
     scaler = torch.cuda.amp.GradScaler()
 
@@ -73,15 +97,20 @@ def train(rank:int, world_size:int,
     train_dataset = DynamicBatchDataset(
         model_name='swin_b', size=3680 if real_data else 1000, max_batch_size=batch_size, 
         hook=hook, trace=None,
-        input_shape=(3, 224, 224), num_class=num_class,
+        input_shape=(3, 32, 32), num_class=num_class,
         fake_data=not real_data,
         max_global_batch_size=global_batch_size,
         checkpoint_micro_batch=checkpoint_micro_batch)
 
     model.train()
     hook.train_start()
+    
+    transform0 = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+    ])
 
-    torch_col.util.initialize_sgd_optimizer(model, optimizer)
+    # torch_col.util.initialize_sgd_optimizer(model, optimizer)
     if train_dataset.checkpoint_micro_batch:
         grad_accumulator = torch_col.GradAccumulator(model)
     else:
@@ -98,12 +127,13 @@ def train(rank:int, world_size:int,
         images = batch['image']
         targets = batch['label']
         images: torch.Tensor = images.cuda(rank, non_blocking=True)
+        images = transform0(images)
         targets: torch.Tensor = targets.cuda(rank, non_blocking=True)
         with train_dataset.get_context(model):
             # if grad_accumulator is not None:
             optimizer.zero_grad(set_to_none=False)
             with torch.cuda.amp.autocast(cache_enabled=False):
-                output = model(images)
+                output: torch.Tensor = model(images)
                 loss = criterion(output, targets)
                 train_dataset.scale_loss(loss)
                 # torch_col.info(f"Batch Size is {len(images)}.")
@@ -119,11 +149,35 @@ def train(rank:int, world_size:int,
         val_labels = np.load('imagenet-100/imagenet-100-labels-val.npy', mmap_mode='c')
         val_dataset = TensorDataset(torch.tensor(val_images, dtype=torch.float32), torch.tensor(val_labels, dtype=torch.long))
         val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    elif os.environ.get('COL_CIFAR100', '0') == '1':
+        # 下载并加载 CIFAR-100 验证数据集
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        print('==> Preparing data..')
+        # 加载验证数据集
+        val_dataset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
+
+        # 提取图像和标签数据
+        val_images = []
+        val_labels = []
+
+        for image, label in val_dataset:
+            val_images.append(image)
+            val_labels.append(label)
+
+        # 将列表转换为张量
+        val_images = torch.stack(val_images)
+        val_labels = torch.tensor(val_labels, dtype=torch.long)
+
+        # 创建 TensorDataset 和 DataLoader
+        val_dataset = TensorDataset(val_images, val_labels)
+        val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
     for epoch in range(num_epoch):
         last_loss = trainer.train_one_epoch(epoch)
         lr = optimizer.param_groups[0]['lr']
         # scheduler.step(epoch)   
-        scheduler.step(epoch)
+        # scheduler.step(epoch)
         epoch_stat = trainer.get_last_epoch_stat()
         epoch_duration = trainer.get_last_epoch_duration()
 
@@ -136,16 +190,40 @@ def train(rank:int, world_size:int,
                 f' | try {epoch_stat.tried_batch} kill {epoch_stat.killed_batch}, ' 
                 + f'{epoch_stat.killed_time*1e3:.1f}ms finish {epoch_stat.finished_batch}, '
                 + f'{epoch_stat.finished_time*1e3:.1f}ms')
-        if os.environ.get('COL_IMAGENET', '0') == '1':
-            val_loss = 0.0
-            with torch.no_grad():
-                for images, labels in val_loader:
-                    images = torch.tensor(images).cuda(non_blocking=True)
-                    labels = torch.tensor(labels).cuda(non_blocking=True)
-                    outputs = model(images)
-                    val_loss += criterion(outputs, labels).item()
-            val_loss /= len(val_loader)
-            batch_info += f' | val {val_loss:.6f}'
+        # time.sleep(10)
+        # torch.cuda.synchronize()
+        # torch_col.dist.wait_barrier()
+        # torch_col.info('Before Swin Buffer1', cast(swin_b_tiny.SwinTransformer, model.module).swin_block1.attn.get_buffer('relative_position_index').cpu())
+        # torch_col.info('Before Swin Buffer2', cast(swin_b_tiny.SwinTransformer, model.module).swin_block2.attn.get_buffer('relative_position_index').cpu())
+        if os.environ.get('COL_IMAGENET', '0') == '1' or os.environ.get('COL_CIFAR100', '0') == '1':
+            while True:
+                total_loss = 0.0
+                correct = 0
+                try:
+                    # with contextlib.nullcontext():
+                    with hook.steps_no_interrupt():
+                        with torch.no_grad():
+                            for images, labels in val_loader:
+                                images = torch.tensor(images).cuda()
+                                labels = torch.tensor(labels).cuda()
+                                outputs: torch.Tensor = model.module(images)
+                                loss = criterion(outputs, labels)
+                                # torch.cuda.synchronize()
+                                correct += (outputs.argmax(1) == labels).type(torch.float).sum().item()
+                                total_loss += loss.item()
+                except torch_col.ColocateAdjustL1Exception as e:
+                    # not calculate success retry
+                    print(f'Exception in val : {e}, retry.')
+                    hook.release_and_reply()
+                    continue
+                break
+            torch_col.info('After Swin Buffer1', cast(swin_b_tiny.SwinTransformer, model.module).swin_block1.attn.get_buffer('relative_position_index').cpu())
+            torch_col.info('After Swin Buffer2', cast(swin_b_tiny.SwinTransformer, model.module).swin_block2.attn.get_buffer('relative_position_index').cpu())
+                # torch_col.dist.wait_barrier()
+            val_loss = total_loss / len(val_loader)
+            val_acc = correct / len(val_loader.dataset)
+            batch_info += f' | val loss {val_loss:.6f} | val acc {val_acc:.6f}'
+        # torch_col.dist.wait_barrier()
         if global_batch_size is not None:
             batch_info += f' | num_rollback_sampels {train_dataset.num_rollback_samples_in_epoch}'
         print('[Rank {} | {} epoch {}] {:.3f}s | {} | batch-size {} | micro-batch-size {} | {} | thpt {:.2f} | lr {:.2e} | loss {:.6f}'.format(
@@ -186,7 +264,7 @@ def train(rank:int, world_size:int,
 def main():
     parser = argparse.ArgumentParser('Train Resnet')    
     parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--global-batch-size', type=int, default=1024)
+    parser.add_argument('--global-batch-size', type=int, default=512)
     parser.add_argument('--num-epoch', type=int, default=15)
     parser.add_argument('--train-mode', type=str, default=TrainMode.COLOCATE_L1.value, 
                         choices=[train_mode.value for train_mode in TrainMode])
@@ -207,7 +285,7 @@ def main():
         + f", num-epoch={num_epoch}, train-mode={train_mode}")
     
     process_context =  torch_mp.spawn(train, 
-                   args=(torch.cuda.device_count(),train_mode,
+                   args=(torch.cuda.device_count(), train_mode,
                          num_epoch, real_data, batch_size, global_batch_size), 
                    nprocs=torch.cuda.device_count(), 
                    join=False)
