0a79c400ca6af523b4e5ada35cd376ad3c9a17b6
Yankui Wang <yankui87@sjtu.edu.cn>
2024-10-14 15:18:01 +0800

    upload triton test script

--- eval/test_triton.py
+++ eval/test_triton.py
@@ -9,28 +9,29 @@ import workload_collections as wkld_coll
 import run_comm
 
 # run_comm.UniformConfig_v2.train_model = 'swin_b_ddp'
-run_comm.UniformConfig_v2.train_batch_size = 64
+run_comm.UniformConfig_v2.train_batch_size = 32
 run_comm.UniformConfig_v2.train_global_batch_size = 512
-run_comm.UniformConfig_v2.duration = 120
+run_comm.UniformConfig_v2.duration = 60
+
 run_comm.UniformConfig_v2.real_data = True
 run_comm.UniformConfig_v2.model_list = ['resnet152']
-run_comm.UniformConfig_v2.num_model = 1
+run_comm.UniformConfig_v2.num_model = 8
 if torch.cuda.device_count() > 1:
     run_comm.UniformConfig_v2.train_model += "_ddp"
     run_comm.UniformConfig_v2.wait_train_setup_sec = 60
-dynamic_sm_partition = True
+dynamic_sm_partition = False
 skip_set_mps_pct = False
 wkld_type = 'NormalA'
 
 system_config = {
-    'mode' : System.ServerMode.ColocateL1,
+    'mode' : System.ServerMode.Normal,
     'use_sta' : True, 
     'mps' : True, 
     'skip_set_mps_thread_percent': skip_set_mps_pct,
-    'use_xsched' : True,
+    'use_xsched' : False,
     'has_warmup' : True,
     'ondemand_adjust' : True,
-    'cuda_memory_pool_gb' : "12.5",
+    'cuda_memory_pool_gb' : "8.0",
     'train_memory_over_predict_mb' : 1500,
     'infer_model_max_idle_ms' : 5000,
     'max_live_minute' : 10 + 7200 // 60,
