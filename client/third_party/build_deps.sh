8c613854cf7bf6281c7b9ccabeb3da3ffaf25043
Yankui Wang <yankui87@sjtu.edu.cn>
2024-10-14 15:14:22 +0800

    upload dependency build script

--- client/third_party/build_deps.sh
+++ client/third_party/build_deps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# 获取脚本的完整路径
+SCRIPT_PATH=$(readlink -f "$0")
 
-SCRIPT_DIR=$(dirname "$(realpath "\$0")")
+# 获取脚本所在的目录
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
 mkdir -p "$SCRIPT_DIR/build"
 cmake -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -DCMAKE_BUILD_TYPE=Release
 
 make -C "$SCRIPT_DIR/build" -j32
+
