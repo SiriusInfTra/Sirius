872881836b4284d5115daaf8ea7fac372b826a12
wangjl <anlarry@foxmail.com>
2024-11-21 12:03:02 +0800

    matrix method

--- util/modeling/matrix_method.py
+++ util/modeling/matrix_method.py
@@ -2,19 +2,36 @@ import scipy
 import numpy as np
 import math
 
-rps = 100 / 56                      # \lambda
-service_time = 0.010                # E[S]
-service_rate = 1 / service_time     # \mu
-live_time = 0.001                       # t
-cold_start_time = 0.050             # L
-hite_rate = 0 / 12                  # a
 
-slo = 0.030                         # SLO
+class hypoexp:
+    def __init__(self, lambdas):
+        self.lambdas = lambdas
+        self._Theta = np.zeros((len(lambdas), len(lambdas)))
+        for i in range(len(lambdas)):
+            self._Theta[i, i] = -lambdas[i]
+            if i + 1 != len(lambdas):
+                self._Theta[i, i + 1] = lambdas[i]
+        
+    def cdf(self, x):
+        tail_prob = scipy.linalg.expm(x * self._Theta)[0, :] @ np.ones(len(self.lambdas))
+        return 1 - tail_prob
+
+
+rps = 5 / 56                      # \lambda
+service_time = 0.0084                # E[S]
+service_rate = 1 / service_time     # \mu
+live_time = 5                       # t
+cold_start_time = 0.0159             # L
+cold_start_rate = 1 / cold_start_time
+hite_rate = 1 / 12                  # a
+
+slo = 0.0293                         # SLO
 
 L_0 = np.array([
     [-(1 / live_time + rps),  1 / live_time],
     [0,                       -rps         ]
 ])
+print(f'Matrix L_0: \n', L_0, end='\n\n')
 
 L = np.array([
     [-(rps + service_rate), 0                           ],
@@ -25,11 +42,13 @@ B = np.array([
     [rps, 0],
     [0,   0]
 ])
+print(f'Matrix B: \n', B, end='\n\n')
 
 F_0 = np.array([
     [rps,             0                   ],
     [hite_rate * rps, (1 - hite_rate) * rps]
 ])
+print(f'Matrix F_0: \n', F_0, end='\n\n')
 
 F = np.array([
     [rps,   0],
@@ -41,34 +60,53 @@ L_rev = np.linalg.inv(L)
 # begin iteration
 R_0 = np.zeros((2, 2))
 R_1 = - (R_0 @ R_0 @ B + F) @ L_rev
-while np.linalg.norm(R_1 - R_0) > 1e-7:
+# while np.linalg.norm(R_1 - R_0, ord=) > 1e-8:
+while np.max(np.abs(R_1 - R_0)) > 1e-8:
     R_0 = R_1
     R_1 = - (R_0 @ R_0 @ B + F) @ L_rev
 
 print(f'Matrix R: \n', R_1, end='\n\n')
 R = R_1
 
-Phi = L_0 + R_1 @ B
-Psi = np.linalg.inv(np.eye(2) - R) @ np.ones((2, 1))
-print(f'Matrix Phi: \n', Phi, end='\n\n')
-print(f'Matrix Psi: \n', Psi, Psi.shape, end='\n\n')
 
-E = np.array([
-    [Psi[0, 0], Phi[0, 1]],
-    [Psi[1, 0], Phi[1, 1]]
+Phi_11 = L + R @ B
+Phi = np.block([
+    [L_0, F_0],
+    [B,   Phi_11]
 ])
 
-pi0 = np.array([1, 0]) @ np.linalg.inv(E)
+Psi = np.block([
+    [np.ones((2, 1))],
+    [np.linalg.inv((np.eye(2) - R)) @ np.ones((2, 1))]
+])
 
-# pi = [i_warm, i_cold]
-print(f'pi_0 = {pi0}')
+# Psi = np.linalg.inv(np.eye(2) - R) @ np.ones((2, 1))
+print(f'Matrix Phi {Phi.shape}: \n', Phi, end='\n\n')
+print(f'Matrix Psi {Psi.shape}: \n', Psi, end='\n\n')
 
-pi_list = [pi0]
+# E = np.array([
+#     [Psi[0, 0], Phi[0, 1]],
+#     [Psi[1, 0], Phi[1, 1]]
+# ])
+E = np.hstack([Psi, Phi[:, 1:]])
+print(f'Matrix E: \n', E, end='\n\n')
+
+# pi0 = np.array([1, 0]) @ np.linalg.inv(E)
+# pi1 = 
+init_pi = np.array([1, 0, 0, 0]) @ np.linalg.inv(E)
+pi_0, pi_1 = init_pi[:2], init_pi[2:]
+
+# pi_1[1] = 0
+
+# # pi = [i_warm, i_cold]
+print(f'init_pi = {init_pi}, \n\npi_0 = {pi_0}, \npi_1 = {pi_1}')
+
+pi_list = [pi_0, pi_1]
 
 R_power_i = R
-upper_bound = 10
-for i in range(1, upper_bound):
-    pi_i = pi0 @ R_power_i
+upper_bound = 20
+for i in range(2, upper_bound):
+    pi_i = pi_1 @ R_power_i
     R_power_i = R_power_i @ R
     print(f'pi_{i} = {pi_i}')
     pi_list.append(pi_i)
@@ -80,14 +118,19 @@ for i in range(upper_bound):
     pi_w, pi_c = pi
     if i == 0:
         # warm
-        if service_time < slo:
-            prob += pi_w
+        prob += pi_w * (1 - math.exp(-service_rate * slo))
         # cold
-        if service_time + cold_start_time < slo:
-            prob += pi_c
+        prob += pi_c * (
+            hite_rate * (1 - math.exp(-service_rate * slo)) +
+            (1 - hite_rate) * hypoexp([service_rate, cold_start_rate]).cdf(slo)
+        )
     else:
         # warm
-        if service_time * (i - 1) + 
+        prob += pi_w * hypoexp([service_rate] * (i + 1)).cdf(slo)
+        # cold
+        prob += pi_c * hypoexp([service_rate] * (i + 1) + [cold_start_rate]).cdf(slo)
+
+print(f'P(slo) = {prob}')
 
 
 
