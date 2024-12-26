e7b801fe457520dc896d6a58f3ca6c819bf43e34
Jiali Wang <anlarry@foxmail.com>
2024-12-26 15:00:54 +0000

    llm

---
+++ server/llm/llm.py
@@ -0,0 +1,28 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
+import torch
+import typing
+    
+
+class LLMInference:
+    def __init__(self, 
+                 model_name: str,
+                 max_length: int = 512, 
+                 max_batch_size: int = None):
+        self.model_name = model_name
+        self.max_length = max_length
+        self.max_batch_size = max_batch_size
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.flight_requests = []
+        self.queueing_requests = []
+        
+    def enqueue_infer(self,
+                      prompt: str):
+        
+
+    
+
+        
+        
+        
